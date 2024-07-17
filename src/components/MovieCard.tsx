a9c60c490b73474e49432783bd876a01d410fc58
Alfred Apenteng <alfredapenteng6996@gmail.com>
2024-07-17 14:02:09 +0000

    Completed Fetcher Functions & Added Movie Card

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const MovieCards = () => {
+const MovieCard = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
       <div className="shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full">
@@ -29,4 +29,4 @@ const MovieCards = () => {
   );
 };
 
-export default MovieCards;
+export default MovieCard;
