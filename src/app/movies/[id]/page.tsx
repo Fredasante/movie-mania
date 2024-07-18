4c4af7fbfd2caf72f4c2a2fcf356bb7d9dd5177a
Alfred Apenteng <alfredapenteng6996@gmail.com>
2024-07-18 14:06:28 +0000

    Added Movie Videos to Single Movie Page

--- src/app/movies/[id]/page.tsx
+++ src/app/movies/[id]/page.tsx
@@ -1,3 +1,4 @@
+import MovieVideos from "@/components/MovieVideos";
 import { Movie } from "@/types/movie";
 import { getMovieDetails } from "@/utils/tmdb";
 import Image from "next/image";
@@ -11,41 +12,45 @@ const page = async ({ params: { id } }: { params: Params }) => {
   const movieDetail: Movie = await getMovieDetails(id);
 
   return (
-    <div className="grid sm:grid-cols-2 items-center w-full max-w-5xl rounded-lg overflow-hidden mx-auto my-6 md:my-10 2xl:my-16">
-      <div className="h-[430px]">
-        <Image
-          src={`https://image.tmdb.org/t/p/w500/${movieDetail.poster_path}`}
-          alt={movieDetail.title}
-          height={500}
-          width={500}
-          className="h-full w-full object-contain"
-        />
-      </div>
+    <div className="container">
+      <section className="grid sm:grid-cols-2 items-center w-full max-w-5xl rounded-lg overflow-hidden mx-auto my-6 md:my-10 2xl:my-16">
+        <div className="h-[430px]">
+          <Image
+            src={`https://image.tmdb.org/t/p/w500/${movieDetail.poster_path}`}
+            alt={movieDetail.title}
+            height={500}
+            width={500}
+            className="h-full w-full object-contain"
+          />
+        </div>
 
-      <div className="p-6 text-gray-100">
-        <h1 className="h2">{movieDetail.title}</h1>
+        <div className="p-6 text-gray-100">
+          <h1 className="h2">{movieDetail.title}</h1>
 
-        <div className="flex gap-4 text-sm font-bold">
-          <span className="text-primary-100">{movieDetail.release_date}</span>
-          <div className="text-gray-200 ml-7 flex items-center">
-            <FaThumbsUp className="text-teal-500 mr-1" />
-            {movieDetail.vote_count.toLocaleString()} votes
+          <div className="flex gap-4 text-sm font-bold">
+            <span className="text-primary-100">{movieDetail.release_date}</span>
+            <div className="text-gray-200 ml-7 flex items-center">
+              <FaThumbsUp className="text-teal-500 mr-1" />
+              {movieDetail.vote_count.toLocaleString()} votes
+            </div>
+          </div>
+
+          <p className="my-5">{movieDetail.overview}</p>
+
+          <div>
+            {movieDetail.genres.map((genre: { id: number; name: string }) => (
+              <span
+                key={genre.id}
+                className="inline-block bg-gray-200 text-slate-700 px-3 py-1 text-sm font-semibold mr-2"
+              >
+                {genre.name}
+              </span>
+            ))}
           </div>
         </div>
+      </section>
 
-        <p className="my-5">{movieDetail.overview}</p>
-
-        <div>
-          {movieDetail.genres.map((genre: { id: number; name: string }) => (
-            <span
-              key={genre.id}
-              className="inline-block bg-gray-200 text-slate-700 px-3 py-1 text-sm font-semibold mr-2"
-            >
-              {genre.name}
-            </span>
-          ))}
-        </div>
-      </div>
+      <MovieVideos id={id} />
     </div>
   );
 };
---
+++ src/components/MovieVideos.tsx
@@ -0,0 +1,76 @@
+import { MovieVideo } from "@/types/movie";
+import { getMovieVideos } from "@/utils/tmdb";
+
+interface MovieVideosProps {
+  id: string;
+}
+
+const MovieVideos = async ({ id }: MovieVideosProps) => {
+  const videos: MovieVideo[] = await getMovieVideos(id);
+
+  // videos that are official trailers
+  const officialTrailers = videos.filter(
+    (video) => video.type === "Trailer" && video.site === "YouTube"
+  );
+
+  // Related videos (excluding the official trailer)
+  const relatedVideos = videos
+    .filter((video) => video.type !== "Trailer")
+    .slice(0, 6);
+
+  if (!officialTrailers.length && !relatedVideos.length) {
+    return (
+      <p className="text-gray-200 text-lg">
+        No videos available for this movie.
+      </p>
+    );
+  }
+
+  return (
+    <div className="my-8 lg:my-12 p-3 text-gray-50">
+      {officialTrailers.length > 0 && (
+        <div className="mb-8 lg:mb-12">
+          <h2 className="text-xl md:text-2xl font-bold mb-4">
+            Official Trailer
+          </h2>
+          <div className="video-container">
+            <iframe
+              width="100%"
+              height="315"
+              src={`https://www.youtube.com/embed/${officialTrailers[0].key}`}
+              title={officialTrailers[0].name}
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+            <p className="mt-2">{officialTrailers[0].name}</p>
+          </div>
+        </div>
+      )}
+
+      {relatedVideos.length > 0 && (
+        <div>
+          <h3 className="text-xl md:text-2xl font-bold mb-5 ">
+            Related Videos
+          </h3>
+          <div className="grid gap-7 lg:gap-10 sm:grid-cols-2 lg:grid-cols-3">
+            {relatedVideos.map((video) => (
+              <div key={video.id} className="video-container">
+                <iframe
+                  width="100%"
+                  height="200"
+                  src={`https://www.youtube.com/embed/${video.key}`}
+                  title={video.name}
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+                <p className="mt-3">{video.name}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MovieVideos;
