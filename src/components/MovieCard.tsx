2fa5b94ce9347234ba774d1fe7d1106aedec07d5
Alfred Apenteng <alfredapenteng6996@gmail.com>
2024-07-18 14:38:28 +0000

    Fix: image display error on homepage

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -21,7 +21,7 @@ const MovieCard = ({ movies }: MovieCardProps) => {
               alt={movie.title}
               width={200}
               height={200}
-              className="h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300"
+              className="w-full object-cover hover:scale-105 transition-scale duration-300"
             />
             <div className="p-3 flex flex-col gap-2 w-full">
               <p className="truncate text-lg font-semibold text-gray-50">
